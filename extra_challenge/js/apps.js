d511256c05740a1e8572fa3c448b429a9f7e37d7
agraham <agraham0925@gmail.com>
2018-03-21 08:35:49 -0500

    2. complete

--- extra_challenge/js/apps.js
+++ extra_challenge/js/apps.js
@@ -7,18 +7,11 @@ console.log(tagMap[0]["Language"])
 
 // 2. Filter out all objects where the Language property does not equal to en (stands for English)
 
-// function filterLanguage (language) {
-
-// 	return language === tag.Map.length tagMap[i][Language]
-
-// 	tagMap[i.filter(filterLanguage(query)
-// }
-
 let filteredList = [];
 
 for(let i = 0; i < tagMap.length; i++){
 	if(tagMap[i]["Language"] === 'en'){
-		console.log(filteredList.push(tagMap[i]));	
+		console.log(filteredList.concat(tagMap[i]));	
 	}
 }
 
