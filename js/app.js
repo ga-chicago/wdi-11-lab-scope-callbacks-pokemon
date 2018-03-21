9d9cf0812598cf51dfeab6ef9cdcc6c797a2c3df
Noemi Romero <nlll02@yahoo.com>
2018-03-21 08:24:30 -0500

    fourth

--- js/app.js
+++ js/app.js
@@ -1,13 +1,60 @@
 console.log("homework")
 
 
-const library = [{name: "Bulbasaur", damage:60}, {name: "Caterpie", damage:40},{name: "Charmander", damage:60},
-{name: "Clefairy", damage:50},{name: "Jigglypuff", damage:60},{name: "Mankey", damage:30},
-{name: "Meowth", damage:60},{name: "Nidoran - female", damage:60},{name: "Nidoran - male", damage:50},
-{name: "Oddish", damage:40},{name: "Pidgey", damage:50},{name: "Pikachu", damage:50},{name: "Poliwag", damage:50},
-{name: "Psyduck", damage:60},{name: "Rattata", damage:30},
-{name: "Squirtle", damage:60}, {name: "Vulpix", damage:50}, {name: "Weedle", damage:40}];
+
+// class Cardsinplay {
+// 	constructor(name, damage)
+// 	}
+// 	cardCount(){
+
+// 	}
+// }
+
+const library = [{name: "Bulbasaur", damage:60}, 
+{name: "Caterpie", damage:40},
+{name: "Charmander", damage:60},
+{name: "Clefairy", damage:50},
+{name: "Jigglypuff", damage:60},
+{name: "Mankey", damage:30},
+{name: "Meowth", damage:60},
+{name: "Nidoran - female", damage:60},
+{name: "Nidoran - male", damage:50},
+{name: "Oddish", damage:40},
+{name: "Pidgey", damage:50},
+{name: "Pikachu", damage:50},
+{name: "Poliwag", damage:50},
+{name: "Psyduck", damage:60},
+{name: "Rattata", damage:30},
+{name: "Squirtle", damage:60}, 
+{name: "Vulpix", damage:50}, 
+{name: "Weedle", damage:40}];
 console.log(library)
+for(rows of library) {
+	// console.log(row)
+	for (elements of rows) {
+		console.log(elements)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+// if (Array.indexOf === undefined) {
+//     // doens't exist in oldIE
+//     /* Finds the index of the first occurence of item in the array, or -1 if not found */
+//     Array.prototype.indexOf = function(v) {
+//         for (let i = 0; i < this.length; ++i) {
+//             if (this[i] === v) {
+//                 return i;
+//             }
+//         }
 
 // playingCards.prototype.draw = function() {
 //         return this.cards.length > 0 ? this.cards.pop() : null;
@@ -22,104 +69,35 @@ console.log(library)
      * get the number of cards remaining in the deck
      * (easy enough just to call cardObject.cards.length but hey)
      */
-    playingCards.prototype.count = function() {
-        return this.cards.length;
-    };
-    /**
-     * shuffle the deck
-     *
-     * @param int n The number of times to shuffle (defaults to 5)
-     */
-    playingCards.prototype.shuffle = function(n) {
-        if (!n) {
-            n = 5;
-        }
-        var l = this.cards.length,
-            r,tmp,i,j;
+    // playingCards.prototype.count = function() {
+    //     return this.cards.length;
+    // };
+    // /**
+    //  * shuffle the deck
+    //  *
+    //  * @param int n The number of times to shuffle (defaults to 5)
+    //  */
+    // playingCards.prototype.shuffle = function(n) {
+    //     if (!n) {
+    //         n = 5;
+    //     }
+    //     var l = this.cards.length,
+    //         r,tmp,i,j;
 
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < l; j++) {
-                r = Math.floor(Math.random() * l);
-                tmp = this.cards[j];
-                this.cards[j] = this.cards[r];
-                this.cards[r] = tmp;
-            }
-        }
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //     for (i = 0; i < n; i++) {
+    //         for (j = 0; j < l; j++) {
+    //             r = Math.floor(Math.random() * l);
+    //             tmp = this.cards[j];
+    //             this.cards[j] = this.cards[r];
+    //             this.cards[r] = tmp;
+    //         }
+    //     }
+    // };
 
  // for(row of name) {
  // 	 console.log(row)
  // for (element of row) {
  // 	console.log(element)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	//}
 // const arrayCount = () =>{
 // 	console.log("yess")
@@ -127,20 +105,6 @@ console.log(library)
 // const arrOfCards = library.name;
 // console.log(arrOfCards)
 // // }
-	
-
-
-
-
-
-
-
-	for (let i = 0; i >= library.length; i++) {
-	library[i].name
-	}
-
-
-
 // const foo77 = [
 // 	["0, 0", "0, 1", "0, 2"],
 // 	["1, 0", "1, 1", "1, 2"], 
