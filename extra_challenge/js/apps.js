e45a26916b554c60cb1fbc6bceaac6ca677ed8fe
agraham <agraham0925@gmail.com>
2018-03-21 06:47:16 -0500

    working on filtered list

--- extra_challenge/js/apps.js
+++ extra_challenge/js/apps.js
@@ -1,12 +1,21 @@
 console.log('test test')
-
+console.log(tagMap[0]["Language"])
 // 1. Iterate over each item in the array
-for(let i = 0; i < tagMap.length; i++){
-			console.log(tagMap[i]);	
-}	
-
+// for(let i = 0; i < tagMap.length; i++){
+// 			console.log(tagMap[i]);	
+// }	
 
 // 2. Filter out all objects where the Language property does not equal to en (stands for English)
+
+function filterLanguage (query) {
+	return tagMap.filter(filterLanguage(query)
+}
+
+let filteredList = tagMap.filter(filterLanguage('en'));
+
+for(let i = 0; i < filteredList.length; i++){
+			console.log(filteredList[i]);	
+}
 
 // 3. Loop over the filtered array and...
 
