a76870b116746600c24c293667bfb70149924375
agraham <agraham0925@gmail.com>
2018-03-21 06:26:05 -0500

    got database functioning

--- extra_challenge/js/apps.js
+++ extra_challenge/js/apps.js
@@ -1,1 +1,17 @@
 console.log('test test')
+
+// 1. Iterate over each item in the array
+for(let i = 0; i < tagMap.length; i++){
+			console.log(tagMap[i]);	
+}	
+
+
+// 2. Filter out all objects where the Language property does not equal to en (stands for English)
+
+// 3. Loop over the filtered array and...
+
+// 4. Output each array item's key and value
+
+// Hint: Remember, this is an array of objects. This means you'll need to use both array methods and 
+// object concepts to complete this. Also remember that some methods return new copies of arrays instead 
+// of changing their values in place
