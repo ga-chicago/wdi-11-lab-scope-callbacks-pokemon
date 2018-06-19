2940b3303f01f44db8d40606fbb646ceef6ed4d3
Badal Moradia <badal@i360llc.com>
2018-06-19 18:48:04 -0500

    added deal card function

--- js/app.js
+++ js/app.js
@@ -19,22 +19,25 @@ const absoluteLibrary = [{name: "Bulbasaur", damage:60},
 				{name: "Squirtle", damage:60}, 
 				{name: "Vulpix", damage:50}, 
 				{name: "Weedle", damage:40}];
-
+//set the totalCards initial condition as the total cards of the aboslute library
 const totalCards = absoluteLibrary.length;
 
+//cardsLeftToPlay intially set to match the absolute library
 const cardsLeftToPlay = absoluteLibrary;
 
+//setup the intial array that will hold all the cards that have been played
 const allCardsPlayed = [];
 
+//set the intial roundsplayed counter to 0
 let roundsPlayed = 0;
 
-
+//setup the object player hand. this will hold information about the player including: name, score, current hand, cards played and methods to adjust current hand and score
 const playerHand = {
 	name: 'Fred',//window.prompt('Please enter your name'),
 	score: 0,
-	currentHand: [1,2,3,4,5],
+	currentHand: [],
 	cardsPlayed: [],
-	roundsWon = 0,
+	roundsWon: 0,
 	playCard(cardNumber){
 		//console.log(this.currentHand[cardNumber-1]);
 		this.cardsPlayed.push(this.currentHand[cardNumber-1]);
@@ -47,14 +50,13 @@ const playerHand = {
 	}
 }
 
-
-
+//setup the object computer hand. this will hold information about the player including: name, score, current hand, cards played and methods to adjust current hand and score
 const computerHand = {
 	name: 'Watson',
 	score: 0,
 	currentHand: [],
 	cardsPlayed: [],
-	roundsWon = 0,
+	roundsWon: 0,
 	playCard(cardNumber){
 		//console.log(this.currentHand[cardNumber-1]);
 		this.cardsPlayed.push(this.currentHand[cardNumber-1]);
@@ -66,6 +68,30 @@ const computerHand = {
 		this.roundsWon++;
 	}
 }
+
+//deals cards to both the player and the computer
+const dealCard = (numberOfCards) => {
+	if(numberOfCards * 2 > cardsLeftToPlay.length){
+		console.error("There aren't enough cards to deal out");
+		return
+	}
+
+	for(let i = 0; i < numberOfCards; i++) {
+		const firstRandom = randomNumber(cardsLeftToPlay.length);
+		playerHand.currentHand.push(cardsLeftToPlay[firstRandom]);
+		cardsLeftToPlay.splice(firstRandom, 1);
+		const secondRandom = randomNumber(cardsLeftToPlay.length);
+		computerHand.currentHand.push(cardsLeftToPlay[secondRandom]);
+		cardsLeftToPlay.splice(secondRandom, 1);
+	}
+
+}
+
+//generates a random number to use in the dealCard function
+const randomNumber = (maxNumber) => {
+  return Math.floor(Math.random() * Math.floor(maxNumber));
+}
+
 
 
 
@@ -73,8 +99,8 @@ const computerHand = {
 //This will be the container for the game once it gets automated
 //The game will continue to loop through the steps until the end condition which is when there aren't enough cards left to deal to both players
 
-
-/*while(cardsLeftToPlay.length > 1){
+//Last Round will be true until there are less than 1 cards left in the deck
+/*while(lastRound){
 
 
 
